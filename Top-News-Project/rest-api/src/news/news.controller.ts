aecc7dbfb17547330a6b2a01a0ea605c9ae9db0b
MartotoBG <martingav123@gmail.com>
2024-05-18 12:34:47 +0300

    adding a get all news request

--- Top-News-Project/rest-api/src/news/news.controller.ts
+++ Top-News-Project/rest-api/src/news/news.controller.ts
@@ -1,23 +1,31 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { NewsService } from "./news.service";
 import { CreateNewDto } from "./dtos/New.dto";
-import { Error, MongooseError } from "mongoose";
 
 @Controller("news")
 export class NewsController {
     constructor(private newsService: NewsService) { }
 
+    @Get("/")
+    async getNews(){
+        try {
+            const news = await this.newsService.getNews();
+            return news;
+        } catch (error) {
+            return this.newsService.catchError(error);
+        }
+    }
+
     @Post("/create")
+    @UsePipes(new ValidationPipe())
     async createNew(@Body() createNewsDto: CreateNewDto) {
         try {
             const newNew = await this.newsService.createNew(createNewsDto);
             return `You created ${newNew.title}`;
         } catch (error) {
-            if (error instanceof MongooseError) {
-                return error.message
-            } else {
-                return error
-            }
+            return this.newsService.catchError(error);
         }
     }
+
+
 }
--- Top-News-Project/rest-api/src/news/news.service.ts
+++ Top-News-Project/rest-api/src/news/news.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, MongooseError } from "mongoose";
 import { New } from "src/models/New";
 import { CreateNewDto } from "./dtos/New.dto";
 
 @Injectable()
 export class NewsService {
-    constructor(
-        @InjectModel(New.name) private newModel: Model<New>) { }
+    constructor(@InjectModel(New.name) private newModel: Model<New>) { }
+
+    getNews(){
+        const news = this.newModel.find();
+        return news;
+    }
 
     createNew(createNewDto: CreateNewDto) {
-        const newNew = new this.newModel(createNewDto);
-        return newNew.save();
+        const createdNew = new this.newModel(createNewDto).save();
+        return createdNew;
+    }
+
+    catchError(error){
+        if (error instanceof MongooseError) {
+            return error.message
+        } else {
+            return error
+        }
     }
 }
