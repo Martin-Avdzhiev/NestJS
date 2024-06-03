b642ac4e3f5c3a7a702df01f98df5effd2ca4361
MartotoBG <martingav123@gmail.com>
2024-06-03 21:41:51 +0300

    adding animation

--- Top-News-Project/client/src/App.tsx
+++ Top-News-Project/client/src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import MainNews from './components/MainNews/MainNews'
 import Navigation from './components/Navigation/Navigation'
 import SecondNavigation from './components/SecondNavigation/SecondNavigation'
-
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      <Navigation/>
-      <SecondNavigation/>
-      <MainNews/>
+      <Navigation />
+      <SecondNavigation />
+      <Routes>
+        <Route path='/' element={<MainNews />} />
+      </Routes>
     </>
   )
 }
--- Top-News-Project/client/src/components/MainNews/MainNews.tsx
+++ Top-News-Project/client/src/components/MainNews/MainNews.tsx
@@ -1,28 +1,62 @@
-import { useEffect, useState } from "react"
-import { getMainNews } from "../../services/newsService"
+import { useEffect, useState } from "react";
+
+import { getMainNews } from "../../services/newsService";
 import { New } from "../../Types/NewsTypes";
 
 import "./MainNews.css";
 
 export default function MainNews() {
     const [mainNews, setMainNews] = useState<New[]>([]);
+    const [cardScales, setCardScales] = useState<{ [key: string]: number[] }>({});
+    const handleZoomIn = (cardId: string) => {
+        setCardScales((prevScales) => ({
+            ...prevScales,
+            [cardId]: [1.1, 0.8],
+        }));
+    };
+
+    const handleZoomOut = (cardId: string) => {
+        setCardScales((prevScales) => ({
+            ...prevScales,
+            [cardId]: [1, 0.6],
+        }));
+    };
     useEffect(() => {
         getMainNews()
-        .then((data: New[]) => setMainNews(data))
-        .catch((error) => console.log(error));
+            .then((data: New[]) => setMainNews(data))
+            .catch((error) => console.log(error));
 
     }, [])
     return (
         <>
-        <div className="main-new-container">
-            {mainNews.map((x) => (
-                <div key={x._id} className="main-new-card" style={{backgroundImage: `url(${x.imageUrl})`}}>
-                    <div className="main-new-overlay"></div>
-                    <p className={`main-new-parahraph-${x.category}`}>{x.category}</p>
-                    <h2>{x.title}</h2>
+            <div className="main-new-container">
+                {mainNews.map((x) => (
+                    <div className="wrapper-main-new-card">
+
+                        <div
+                            className="main-new-card"
+                            key={x._id}
+                            onMouseEnter={() => handleZoomIn(x._id)}
+                            onMouseLeave={() => handleZoomOut(x._id)}
+                            style={{
+                                backgroundImage: `url(${x.imageUrl})`,
+                                backgroundColor: "black",
+                                transform: `scale(${cardScales[x._id] ? cardScales[x._id][0] : 1})`,
+                                transition: "transform 0.2s ease-in-out"
+                            }}>
+                            <div className="main-new-overlay"
+                                style={{
+                                    opacity: `${cardScales[x._id] ? cardScales[x._id][1] : 0.6}`,
+                                    transition: "opacity 0.2s ease-in-out",
+                                }}
+                            ></div>
+                            <p className={`main-new-parahraph-${x.category}`}>{x.category}</p>
+                            <h2>{x.title}</h2>
+                        </div>
                     </div>
-            ))}
+                ))}
             </div>
         </>
     )
 }
+
