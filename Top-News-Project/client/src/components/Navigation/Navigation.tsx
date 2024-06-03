d9fd367dad16fa9b6ddf5e09b2796363fbb8dda3
MartotoBG <martingav123@gmail.com>
2024-06-04 01:29:13 +0300

    adding navigate on logo

--- Top-News-Project/client/src/components/Navigation/Navigation.tsx
+++ Top-News-Project/client/src/components/Navigation/Navigation.tsx
@@ -1,6 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./Navigation.css";
+
+import { useNavigate } from "react-router-dom";
 export default function Navigation() {
+    const navigate = useNavigate();
+    const navigateToMainPage = () => {
+        navigate("/");
+    }
     return (
         <>
             <div className="main-navigation-div">
@@ -22,7 +29,7 @@ export default function Navigation() {
                 </div>
             </div>
             <div className="main-image">
-                <img src="./voice_logo.png" alt="voice-logo" />
+                <img src="./voice_logo.png" alt="voice-logo" onClick={navigateToMainPage}/>
             </div>
 
         </>
