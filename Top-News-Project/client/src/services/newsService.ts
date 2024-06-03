b846dd45225d66cd319f540d415f0b4e3872313e
MartotoBG <martingav123@gmail.com>
2024-06-03 18:07:30 +0300

    Update newsService.ts

--- Top-News-Project/client/src/services/newsService.ts
+++ Top-News-Project/client/src/services/newsService.ts
@@ -1,7 +1,6 @@
 import { New } from "../Types/NewsTypes";
 
 const url = import.meta.env.VITE_API_URL;
-console.log(url)
 
 export async function getMainNews(){
         const response = await fetch(url);
