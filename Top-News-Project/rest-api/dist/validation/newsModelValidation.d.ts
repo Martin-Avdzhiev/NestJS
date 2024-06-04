daf73b41a0dc1980e1e84f48144ed79de8ce767d
MartotoBG <martingav123@gmail.com>
2024-06-04 18:03:35 +0300

    Update newsModelValidation.d.ts

--- Top-News-Project/rest-api/dist/validation/newsModelValidation.d.ts
+++ Top-News-Project/rest-api/dist/validation/newsModelValidation.d.ts
@@ -1,2 +1,2 @@
-export type Categories = "Technology" | "Environment" | "Food" | "Fashion";
+export type Categories = "Technology" | "Environment" | "Food" | "Fashion" | "Lifestyle" | "Music";
 export declare function isValidCategory(category: string): boolean;
