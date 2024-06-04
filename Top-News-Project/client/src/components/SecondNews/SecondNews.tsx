5e46bca2b23ceda965062580c2a32fa1ecc4ca59
MartotoBG <martingav123@gmail.com>
2024-06-04 19:47:34 +0300

    Update SecondNews.tsx

--- Top-News-Project/client/src/components/SecondNews/SecondNews.tsx
+++ Top-News-Project/client/src/components/SecondNews/SecondNews.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from "react"
+
 export default function SecondNews(){
+    useEffect(() => {
+        
+    },[])
     return (
         <>
         <div className="main-second-news-container">
             <div className="column-container">
-                
+
             </div>
             <div className="column-container">
 
