376cf796900a4bc78756be79a825f2bee91da7dc
MartotoBG <107769673+Martin-Avdzhiev@users.noreply.github.com>
2024-06-14 15:26:25 +0300

    adding animation to images

--- Top-News-Project/client/src/components/MainPage/SecondNews/FeaturedPosts/FeaturedPosts.tsx
+++ Top-News-Project/client/src/components/MainPage/SecondNews/FeaturedPosts/FeaturedPosts.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 
 import { New } from "../../../../Types/NewsTypes";
 import { getLatesNews } from "../../../../services/newsService";
+import { handleZoomIn, handleZoomOut } from "../../../../animations/zoom";
+import { CardScale } from "../../../../Types/AnimationTypes";
 
 import "./FeaturedPosts.css";
 
 export default function FeaturedPosts() {
     const [latestNews, setLatesNews] = useState<New[]>([]);
+    const [cardZoom, setCardZoom] = useState<{ [key: string]: CardScale }>({});
     useEffect(() => {
         getLatesNews(3).then((data) => setLatesNews(data));
     })
@@ -19,11 +22,21 @@ export default function FeaturedPosts() {
 
                         <div className="featured-post-card" key={x._id}>
                             <div className="featured-post-img-conatainer">
-                                <img src={x.imageUrl} alt={x.title} />
+                                <img
+                                    onMouseEnter={() => handleZoomIn(x._id, 1.1, setCardZoom)}
+                                    onMouseLeave={() => handleZoomOut(x._id, 1, setCardZoom)}
+                                    style={{
+                                        transform: `scale(${x._id ? cardZoom[x._id]?.zoom : 1})`,
+                                        transition: "transform 0.3s ease-in-out",
+                                        overflow: "hidden",
+                                        cursor: "pointer",
+                                        width: "100%"
+                                    }}
+                                    src={x.imageUrl} alt={x.title} />
                             </div>
                             <div className="featured-post-info">
-                            <p className={`featured-post-category main-new-parahraph-${x.category}-color`}>{x.category[0].toUpperCase() + x.category.slice(1)}</p>
-                            <h3 className="featured-post-title">{x.title}</h3>
+                                <p className={`featured-post-category main-new-parahraph-${x.category}-color`}>{x.category[0].toUpperCase() + x.category.slice(1)}</p>
+                                <h3 className="featured-post-title">{x.title}</h3>
                             </div>
                         </div>
                     )) : null}
