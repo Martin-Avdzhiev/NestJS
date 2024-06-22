b5ec5819af5b4db7872e86b63751957a1b8cf021
MartotoBG <martingav123@gmail.com>
2024-06-22 18:52:07 +0300

    finishing footer

--- Top-News-Project/client/src/components/Footer/FooterContent/FooterContent.tsx
+++ Top-News-Project/client/src/components/Footer/FooterContent/FooterContent.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { getLatestNews } from "../../../services/newsService";
 import { New } from "../../../Types/NewsTypes";
@@ -10,28 +11,46 @@ export default function FooterContent() {
     const [talkedAbout, setTalkedAbout] = useState<New[]>([]);
     useEffect(() => {
         getLatestNews(4).then((data) => setPopularNews(data));
-        getLatestNews(8).then((data) => setTalkedAbout(data.slice(0,4)));
+        getLatestNews(8).then((data) => setTalkedAbout(data.slice(0, 4)));
     })
 
     return (
         <div className="footer-content-wrapper">
             <div className="footer-content">
                 <div className="footer-card-wrapper">
-                    <h2>Most popular</h2>
+                    <h2 className="footer-container-title">Most popular</h2>
                     <FooterCard news={popularNews} />
                 </div>
                 <div className="footer-card-wrapper">
-                    <h2>Talked about</h2>
+                    <h2 className="footer-container-title">Talked about</h2>
                     <FooterCard news={talkedAbout} />
                 </div>
-                <div className="footer-side-div">
-                    <p>1</p>
-
-                    <div className="footer-video">
-
-
+                <div className="footer-side-container">
+                    <h2 className="footer-container-title">Video of the day</h2>
+                    <div className="video-wrapper">
+                        <iframe
+                            width="318"
+                            height="150"
+                            src="https://www.youtube.com/embed/XsEMu5UCy0g"
+                            title="Irish Stew of Sindidun - Take Me High (Official)"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                        ></iframe>
                     </div>
-                    <div className="footer-join-us"></div>
+                    <div className="footer-join-us">
+                        <h2 className="footer-container-title">Join us</h2>
+                        <div className="footer-socials">
+                            <ul>
+                                <FontAwesomeIcon icon={['fab', 'instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'twitter']} />
+                                <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                                <FontAwesomeIcon icon={['fab', 'youtube']} />
+                                <FontAwesomeIcon icon={['fab', 'pinterest-p']} />
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
             </div>
--- Top-News-Project/client/src/fontawesome.ts
+++ Top-News-Project/client/src/fontawesome.ts
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebookF, faTwitter, faGooglePlusG, faInstagram, faVk } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faGooglePlusG, faInstagram, faYoutube, faPinterestP, faVk } from '@fortawesome/free-brands-svg-icons';
 import { faChevronDown, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 
-library.add(faFacebookF, faTwitter, faGooglePlusG, faVk, faInstagram, faChevronDown, faMagnifyingGlass);
+library.add(faFacebookF, faTwitter, faGooglePlusG, faVk, faInstagram, faChevronDown, faMagnifyingGlass, faYoutube, faPinterestP);
