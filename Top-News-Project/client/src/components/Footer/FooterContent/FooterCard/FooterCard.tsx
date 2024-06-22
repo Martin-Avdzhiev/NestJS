eed71e16a72f17f83480a67f6d17328f5b212ede
MartotoBG <martingav123@gmail.com>
2024-06-22 18:13:25 +0300

    adding second section in the footer

---
+++ Top-News-Project/client/src/components/Footer/FooterContent/FooterCard/FooterCard.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+import { New } from "../../../../Types/NewsTypes";
+import { calculateDaysBeforeDate } from "../../../../services/dateService";
+import { handleZoomIn, handleZoomOut } from "../../../../animations/zoom";
+
+import "./FooterCard.css";
+import { CardScale } from "../../../../Types/AnimationTypes";
+
+export default function FooterCard({ news }: { news: New[] }) {
+    const [cardScales, setCardScales] = useState<{ [key: string]: CardScale }>({});
+    return (
+        <>
+            {news.length > 0 ? news.map((x) => (
+                <div className="footer-card" key={x._id + "qwerty"}>
+                    <div className="footer-card-img-wrapper">
+                        <img
+                            onMouseEnter={() => handleZoomIn(x._id + "qwerty", 1.1, setCardScales)}
+                            onMouseLeave={() => handleZoomOut(x._id + "qwerty", 1, setCardScales)}
+                            style={{
+                                transform: `scale(${cardScales[x._id + "qwerty"] ? cardScales[x._id + "qwerty"].zoom : 1})`,
+                                transition: "transform 0.2s ease-in-out",
+                                cursor: "pointer"
+                            }}
+                            src={x.imageUrl} alt={x.title} />
+                    </div>
+                    <div className="footer-card-info">
+                        <h4
+                            className="footer-card-title red-text-animation"
+                            style={{
+                                transition: "color 0.3s ease-in-out",
+                            }}>
+                            {x.title}
+                        </h4>
+                        <p className="footer-card-timestamp">
+                            {calculateDaysBeforeDate(x.createdAt)}
+                        </p>
+                    </div>
+                </div>
+            )) : null}
+        </>
+    )
+}
--- Top-News-Project/client/src/components/Footer/FooterContent/FooterContent.tsx
+++ Top-News-Project/client/src/components/Footer/FooterContent/FooterContent.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 
 import { getLatestNews } from "../../../services/newsService";
 import { New } from "../../../Types/NewsTypes";
-import { calculateDaysBeforeDate } from "../../../services/dateService";
-
+import FooterCard from "./FooterCard/FooterCard";
 import "./FooterContent.css";
 
 export default function FooterContent() {
@@ -11,34 +10,19 @@ export default function FooterContent() {
     const [talkedAbout, setTalkedAbout] = useState<New[]>([]);
     useEffect(() => {
         getLatestNews(4).then((data) => setPopularNews(data));
-        getLatestNews(7).then((data) => setTalkedAbout(data.slice(4)));
+        getLatestNews(8).then((data) => setTalkedAbout(data.slice(0,4)));
     })
 
     return (
         <div className="footer-content-wrapper">
             <div className="footer-content">
-                <div className="footer-most-popular">
+                <div className="footer-card-wrapper">
                     <h2>Most popular</h2>
-                    {popularNews.length > 0 ? popularNews.map((x) => (
-                        <div className="footer-most-popular-card" key={x._id}>
-                            <div className="footer-card-img-wrapper">
-                                <img src={x.imageUrl} alt={x.title} />
-                            </div>
-                            <div className="footer-card-info">
-                                <h4
-                                    className="footer-card-title red-text-animation"
-                                    style={{
-                                        transition: "color 0.3s ease-in-out",
-                                    }}>
-                                    {x.title}
-                                </h4>
-                                <p className="footer-card-timestamp">{calculateDaysBeforeDate(x.createdAt)}</p>
-                            </div>
-                        </div>
-                    )) : null}
+                    <FooterCard news={popularNews} />
                 </div>
-                <div className="footer-talked-about">
-                    <p>1</p>
+                <div className="footer-card-wrapper">
+                    <h2>Talked about</h2>
+                    <FooterCard news={talkedAbout} />
                 </div>
                 <div className="footer-side-div">
                     <p>1</p>
