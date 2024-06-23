85d44d35c27647105290b3d09bec58dc15c0c8d6
MartotoBG <martingav123@gmail.com>
2024-06-23 16:05:40 +0300

    Update FooterCard.tsx

--- Top-News-Project/client/src/components/Footer/FooterContent/FooterCard/FooterCard.tsx
+++ Top-News-Project/client/src/components/Footer/FooterContent/FooterCard/FooterCard.tsx
@@ -6,17 +6,25 @@ import { handleZoomIn, handleZoomOut } from "../../../../animations/zoom";
 
 import "./FooterCard.css";
 import { CardScale } from "../../../../Types/AnimationTypes";
+import { useNavigate } from "react-router-dom";
 
 export default function FooterCard({ news }: { news: New[] }) {
     const [cardScales, setCardScales] = useState<{ [key: string]: CardScale }>({});
+
+    const navigate = useNavigate();
+    const redirectHandler = (id:string) => {
+        navigate(`/post/${id}`);
+    }
+    
     return (
         <>
             {news.length > 0 ? news.map((x) => (
                 <div className="footer-card" key={x._id + "qwerty"}>
                     <div className="footer-card-img-wrapper">
                         <img
-                            onMouseEnter={() => handleZoomIn(x._id + "qwerty", 1.1, setCardScales)}
-                            onMouseLeave={() => handleZoomOut(x._id + "qwerty", 1, setCardScales)}
+                        onClick={()=> redirectHandler(x._id)}
+                        onMouseEnter={() => handleZoomIn(x._id + "qwerty", 1.1, setCardScales)}
+                        onMouseLeave={() => handleZoomOut(x._id + "qwerty", 1, setCardScales)}
                             style={{
                                 transform: `scale(${cardScales[x._id + "qwerty"] ? cardScales[x._id + "qwerty"].zoom : 1})`,
                                 transition: "transform 0.2s ease-in-out",
@@ -26,6 +34,7 @@ export default function FooterCard({ news }: { news: New[] }) {
                     </div>
                     <div className="footer-card-info">
                         <h4
+                            onClick={()=> redirectHandler(x._id)}
                             className="footer-card-title red-text-animation"
                             style={{
                                 transition: "color 0.3s ease-in-out",
