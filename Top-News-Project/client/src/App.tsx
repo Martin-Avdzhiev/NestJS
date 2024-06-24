ca73f8152b0d44e27bf5d5fe8798e18811fcfecf
MartotoBG <martingav123@gmail.com>
2024-06-24 15:00:44 +0300

    fixing the problem with scroll

--- Top-News-Project/client/src/App.tsx
+++ Top-News-Project/client/src/App.tsx
@@ -6,10 +6,12 @@ import MainPage from './components/Main/MainPage/MainPage';
 import Post from './components/Post/Post';
 
 import { Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/scrollToTop';
 function App() {
 
   return (
     <>
+    <ScrollToTop/>
       <div className="page-container-wrapper">
         <div className="page-container">
           <Navigation />
