c1f51604831726fe736dc0fd0909c2f6d1a71aea
MartotoBG <107769673+Martin-Avdzhiev@users.noreply.github.com>
2024-08-04 18:05:41 +0300

    adding useForm to login component

--- Top-News-Project/client/src/components/Auth/Login.tsx
+++ Top-News-Project/client/src/components/Auth/Login.tsx
@@ -1,15 +1,37 @@
+import { useForm } from "../../hooks/useForm";
+
 import "./Auth.css";
 
-export default function Login(){
+interface data {
+    username: string;
+    password: string;
+    [key: string]: string;
+}
+
+export default function Login() {
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        resetForm();
+    };
+
+    const { formData, handleChange, resetForm }
+        = useForm<data>({ username: "", password: "" });
     return (
         <>
-        <form method="POST" className="auth-form">
-            <div className="form-inputs">
-                <input type="text" placeholder="Username"/>
-                <input type="password" placeholder="Password"/>
-            </div>
-            <button type="submit">Login</button>
-        </form>
+            <form method="POST" className="auth-form" onSubmit={handleSubmit}>
+                <div className="form-inputs">
+                    <input type="text" placeholder="Username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange} />
+                    <input type="password" placeholder="Password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange} />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </>
     )
 }
